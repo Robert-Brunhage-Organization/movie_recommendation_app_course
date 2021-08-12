713709a102709733d1dd30f58f166b71346ea496
Robert Brunhage <robertbrunhage@gmail.com>
2021-08-12 10:01:44 +0200

    Module 1 Lesson 0 - Base Project

---
+++ android/app/src/main/kotlin/com/example/movie_recommendation_app_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.movie_recommendation_app_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movie Recommendation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
