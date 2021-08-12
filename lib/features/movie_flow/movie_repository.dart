54b546f7726841f256828c6151efb365e8f841ec
Robert Brunhage <robertbrunhage@gmail.com>
2021-08-12 13:41:04 +0200

    Module 3 Lesson 3 - Creating a Repository

---
+++ lib/features/movie_flow/movie_repository.dart
@@ -0,0 +1,56 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movie_recommendation_app_course/core/environment_variables.dart';
+import 'package:movie_recommendation_app_course/features/movie_flow/genre/genre_entity.dart';
+import 'package:movie_recommendation_app_course/features/movie_flow/result/movie_entity.dart';
+import 'package:movie_recommendation_app_course/main.dart';
+
+final movieRepositoryProvider = Provider<MovieRepository>((ref) {
+  final dio = ref.watch(dioProvider);
+  return TMDBMovieRepository(dio: dio);
+});
+
+abstract class MovieRepository {
+  Future<List<GenreEntity>> getMovieGenres();
+  Future<List<MovieEntity>> getRecommendedMovies(double rating, String date, String genreIds);
+}
+
+class TMDBMovieRepository implements MovieRepository {
+  TMDBMovieRepository({required this.dio});
+  final Dio dio;
+
+  @override
+  Future<List<GenreEntity>> getMovieGenres() async {
+    final response = await dio.get(
+      'genre/movie/list',
+      queryParameters: {
+        'api_key': api,
+        'language': 'en-US',
+      },
+    );
+    final results = List<Map<String, dynamic>>.from(response.data['genres']);
+    final genres = results.map((e) => GenreEntity.fromMap(e)).toList();
+
+    return genres;
+  }
+
+  @override
+  Future<List<MovieEntity>> getRecommendedMovies(double rating, String date, String genreIds) async {
+    final response = await dio.get(
+      'discover/movie',
+      queryParameters: {
+        'api_key': api,
+        'language': 'en-US',
+        'sort_by': 'popularity.desc',
+        'include_adult': false,
+        'vote_average.gte': rating,
+        'page': 1,
+        'release_date.gte': date,
+        'with_genres': genreIds,
+      },
+    );
+    final results = List<Map<String, dynamic>>.from(response.data['results']);
+    final movies = results.map((e) => MovieEntity.fromMap(e)).toList();
+    return movies;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,6 +8,12 @@ import 'package:movie_recommendation_app_course/theme/custom_theme.dart';
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
+
+final dioProvider = Provider<Dio>((ref) {
+  return Dio(BaseOptions(
+    baseUrl: 'https://api.themoviedb.org/3/',
+  ));
+});
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
